119dde88fe25d69fff3677203059e230925516e7
kd2-sahil-83516 <sahilchaudhari0821@gmail.com>
2024-08-13 22:12:16 +0530

    JWT security is implemented

--- Backend/car_buyand_sell_JWT/src/main/java/com/app/Application.java
+++ Backend/car_buyand_sell_JWT/src/main/java/com/app/Application.java
@@ -5,8 +5,8 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.modelmapper.Conditions;
 
 @SpringBootApplication
@@ -15,5 +15,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
 
 }

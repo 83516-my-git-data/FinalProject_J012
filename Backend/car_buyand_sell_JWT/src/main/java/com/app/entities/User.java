49cf03f8f4b8fa568aadd608b285d44bf67e46fb
kd2-sahil-83516 <sahilchaudhari0821@gmail.com>
2024-08-13 20:33:07 +0530

    entities added

---
+++ Backend/car_buyand_sell_JWT/src/main/java/com/app/entities/User.java
@@ -0,0 +1,42 @@
+package com.app.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String mobileNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
